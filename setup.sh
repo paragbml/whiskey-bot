a8952ccaf36351895f2cc30decc265c9824589ea
paragbml <paragb061@gmail.com>
2025-06-25 02:37:54 +0530

    Add puppeteer-extra setup script for Railway

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Clone puppeteer-extra and install it
 git clone https://github.com/berstend/puppeteer-extra.git
+
+# Install everything including puppeteer-extra
 npm install ./puppeteer-extra/packages/puppeteer-extra
+npm install
